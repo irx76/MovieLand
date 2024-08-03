70b95945715ba628d4af8d848d4fe8c36e396865
irx76 <iromathews4@gmail.com>
2024-08-03 16:23:42 +0530

    React app created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+// we have to export every component we create so that we can import them in other files
+export default App; 
