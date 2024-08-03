ae7e6f37f4849cdf40ef30e362324f30d3c15512
irx76 <iromathews4@gmail.com>
2024-08-03 16:55:56 +0530

    Added Omdb API and key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
 
+
+const API_KEY= "d3fd6995";
+
+const API_URL=`http://www.omdbapi.com/?apikey=${API_KEY}`
 
 const App = () => {
+
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data.Search); 
+    }
+
+
+    useEffect (() => {
+        searchMovies("Harry Potter");
+    },[]);
+
     return(
         <div>
             <h1>Hello World</h1>
